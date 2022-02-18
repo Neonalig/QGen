b5ffaf66ea4f4f1cfa2c7533b746faae85e2d71c
Neonalig <codychook@gmail.com>
2022-02-18 21:15:41 +0800

    Fixed copyright information

--- Core/ScriptGenerator.cs
+++ Core/ScriptGenerator.cs
@@ -1,10 +1,22 @@
-﻿using System.IO;
+﻿#region Copyright (C) 2017-2022  Cody Bock
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License (Version 3.0)
+// as published by the Free Software Foundation.
+// 
+// More information can be found here: https://www.gnu.org/licenses/gpl-3.0.en.html
+#endregion
+
+#region Using Directives
+
+using System.IO;
 using System.Runtime.CompilerServices;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+
+#endregion
 
 namespace QGen.Core;
 
--- QGen.Sample/InputAsset.cs
+++ QGen.Sample/InputAsset.cs
@@ -1,4 +1,12 @@
-﻿namespace QGen.Sample;
+﻿#region Copyright (C) 2017-2022  Cody Bock
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License (Version 3.0)
+// as published by the Free Software Foundation.
+// 
+// More information can be found here: https://www.gnu.org/licenses/gpl-3.0.en.html
+#endregion
+
+namespace QGen.Sample;
 
 public class InputAsset {
 #pragma warning disable CA1822 // Mark members as static
